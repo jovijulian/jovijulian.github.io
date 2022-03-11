bb86ac3e63a19419c8c4cdbc0b1ade06c97f1a71
jovijulian <jovijulian2002@gmail.com>
2022-03-11 20:21:42 +0700

    fix web

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,3 +23,16 @@ form.addEventListener("submit", (e) => {
     })
     .catch((error) => console.error("Error!", error.message));
 });
+
+const typed = document.querySelector(".typed");
+if (typed) {
+  let typed_strings = typed.getAttribute("data-typed-items");
+  typed_strings = typed_strings.split(",");
+  new Typed(".typed", {
+    strings: typed_strings,
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelay: 2000,
+  });
+}
