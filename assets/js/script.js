9d91232d74f4bab7f77b660879449aa6f1dd0fe9
jovijulian <jovijulian2002@gmail.com>
2022-03-15 15:13:34 +0700

    add active navbar ketika diklik dan scroll

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,3 +36,18 @@ if (typed) {
     backDelay: 2000,
   });
 }
+
+const sectionAll = document.querySelectorAll("section[id]");
+window.addEventListener("scroll", () => {
+  const scrollY = window.pageYOffset;
+  sectionAll.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 200;
+    const sectionId = current.getAttribute("id");
+    if (scrollY > sectionTop && scrollY < sectionTop + sectionHeight) {
+      document.querySelector('li a[href*="' + sectionId + '"]').classList.add("active");
+    } else {
+      document.querySelector('li a[href*="' + sectionId + '"]').classList.remove("active");
+    }
+  });
+});
